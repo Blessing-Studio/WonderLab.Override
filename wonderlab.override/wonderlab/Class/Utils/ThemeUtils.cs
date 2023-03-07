c7a8794f5079786e55b4784e78e7ceab6b9aab31
hei-shou <blessing_114514@qq.com>
2023-03-08 00:12:54 +0800

    添加主题工具类

---
+++ wonderlab.override/wonderlab/Class/Utils/ThemeUtils.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wonderlab.Class.Utils
+{
+    public class ThemeUtils {
+        public void SetAccentColor(object key, object value) {
+            if (AccentColorResources.ContainsKey(key)) {
+                AccentColorResources[key] = value;
+            }
+            else {
+                AccentColorResources.Add(key, value);
+            }
+
+        }
+
+        public static readonly ResourceDictionary AccentColorResources = new ResourceDictionary();
+    }
+}
