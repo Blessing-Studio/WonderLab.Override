6adc066caeff9d92cf10e26c02fe52830fcc03cc
hei-shou <blessing_114514@qq.com>
2023-04-16 14:31:06 +0800

    添加了账户刷新逻辑

--- wonderlab.override/wonderlab/ViewModels/Pages/HomePageViewModel.cs
+++ wonderlab.override/wonderlab/ViewModels/Pages/HomePageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MinecraftLaunch.Launch;
+﻿using MinecaftOAuth.Authenticator;
+using MinecraftLaunch.Launch;
 using MinecraftLaunch.Modules.Enum;
 using MinecraftLaunch.Modules.Interface;
 using MinecraftLaunch.Modules.Models.Auth;
@@ -119,6 +120,27 @@ namespace wonderlab.ViewModels.Pages
 
                 Trace.WriteLine($"[信息] Http状态码为 {result.HttpStatusCode}");
             }
+
+            //异步刷新游戏账户
+            await Task.Run(async () => {
+                if(CurrentAccount.Type == AccountType.Yggdrasil) {
+                    YggdrasilAuthenticator authenticator = new YggdrasilAuthenticator((CurrentAccount as YggdrasilAccount)!.YggdrasilServerUrl, "", "");
+                    var result = await authenticator.RefreshAsync((CurrentAccount as YggdrasilAccount)!);
+
+                    CurrentAccount = result;
+                    await GameAccountUtils.RefreshUserDataAsync((await GameAccountUtils.GetUsersAsync().ToListAsync()).Where(x => x.Data.Uuid == result.Uuid.ToString()).First().Data, result);
+                }
+                else if (CurrentAccount.Type == AccountType.Microsoft) {
+                    MicrosoftAuthenticator authenticator = new(MinecaftOAuth.Module.Enum.AuthType.Refresh) {
+                        ClientId = "9fd44410-8ed7-4eb3-a160-9f1cc62c824c",
+                        RefreshToken = (CurrentAccount as MicrosoftAccount)!.RefreshToken!
+                    };
+
+                    var result = await authenticator.AuthAsync(x => Trace.WriteLine($"[信息] 当前验证步骤 {x}"));
+                    CurrentAccount = result;
+                    await GameAccountUtils.RefreshUserDataAsync((await GameAccountUtils.GetUsersAsync().ToListAsync()).Where(x => x.Data.Uuid == result.Uuid.ToString()).First().Data, result);
+                }
+            });
 
             var config = new LaunchConfig()
             {
