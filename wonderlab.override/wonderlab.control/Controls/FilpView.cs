dd3b418ff17f72215ccc969ddd10512a18fc2e6f
hei-shou <blessing_114514@qq.com>
2023-04-23 13:33:48 +0800

    完成轮播图控件

---
+++ wonderlab.override/wonderlab.control/Controls/FilpView.cs
@@ -0,0 +1,65 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wonderlab.control.Controls.Dialog;
+using wonderlab.control.Controls.Dialog.Events;
+
+namespace wonderlab.control.Controls
+{
+    /// <summary>
+    /// 轮播图控件
+    /// </summary>
+    public class FilpView : TemplatedControl {
+        public static readonly StyledProperty<ObservableCollection<IImage>> SourceProperty =
+            AvaloniaProperty.Register<FilpView, ObservableCollection<IImage>>(nameof(Source), new());
+
+        public static readonly StyledProperty<IImage> CurrentProperty =
+            AvaloniaProperty.Register<FilpView, IImage>(nameof(Current));
+
+        public ObservableCollection<IImage> Source { get => GetValue(SourceProperty); set => SetValue(SourceProperty, value); }
+
+        public IImage Current { get => GetValue(CurrentProperty); set => SetValue(CurrentProperty, value); }
+
+        public Button MoveToLeftButton, MoveToRightButton;
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {       
+            base.OnApplyTemplate(e);
+
+            Current = Source is null ? null : Source.First();
+
+            MoveToLeftButton = e.NameScope.Find<Button>("MoveToLeftButton");
+            MoveToRightButton = e.NameScope.Find<Button>("MoveToRightButton");
+
+            MoveToLeftButton.Click += MoveToLeftButton_Click;
+            MoveToRightButton.Click += MoveToRightButton_Click;
+            MoveToLeftButton.IsVisible = false;
+        }
+
+        private void MoveToRightButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)  {      
+            var result = Source.IndexOf(Current);
+            if (result != -1 && result + 1 < Source.Count) {           
+                Current = Source[result + 1];
+
+                MoveToRightButton.IsVisible = !(Current == Source.Last());
+                MoveToLeftButton.IsVisible = true;
+            }
+        }
+
+        private void MoveToLeftButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
+            var result = Source.IndexOf(Current);
+            if(result != -1 && result > 0) {
+                Current = Source[result - 1];
+                MoveToLeftButton.IsVisible = !(Current == Source.First());
+                MoveToRightButton.IsVisible = true;
+            }
+        }
+    }
+}
--- wonderlab.override/wonderlab.control/MainWindow.axaml.cs
+++ wonderlab.override/wonderlab.control/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using wonderlab.control.Controls.Bar;
@@ -10,74 +11,12 @@ namespace wonderlab.control
     {
         public MainWindow()
         {
-            Opened += MainWindow_Opened;
             InitializeComponent();
             ColorHelper helper = new ColorHelper();
             helper.Load();
-            //d.CloseButtonClick += D_CloseButtonClick;
-            button.Click += Button_Click;
-            ThemeChange.Checked += ThemeChange_Checked;
-            bab.LaunchButtonClick += Bab_LaunchButtonClick;
-        }
-
-        private void Bab_LaunchButtonClick(object? sender, System.EventArgs e)
-        {
-            MessageTipsBar bar = new MessageTipsBar(new(Action))
-            {
-                Title = "信息",
-                Message = "开始尝试启动游戏 1.12.2-WonderLab-DemoClient"
-            };             
-            grid.Children.Add(bar);
-            bar.Opened += async (_, _) =>
-            {
-                await Task.Delay(3000);
-                bar.HideDialog();
-            };
-
-            bar.ShowDialog();
-        }
-
-        private void ThemeChange_Checked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-        {
-            ColorHelper helper = new();
-            if (ThemeChange.IsChecked!.Value)
-            {
-                helper.UpdateTheme("Light");
-            }
-            else
-            {
-                helper.UpdateTheme("Dark");
-            }
-        }
-
-        void Action()
-        {
-            Trace.WriteLine("[Info] This MessageTipsBar is Clicked!");
-        }
-
-        private async void MainWindow_Opened(object? sender, System.EventArgs e)
-        {
-            await Task.Delay(1000);
-            tb.InitStartAnimation();
-
-        }
-
-        private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-        {
-            d.ShowDialog();
-        }
-
-        private void ShowDialogClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-        {
-            MessageTipsBar bar = new MessageTipsBar(new(Action));
-            grid.Children.Add(bar);
-            bar.Opened += async (_, _) =>
-            {
-                await Task.Delay(1000);
-                bar.HideDialog();
-            };
-
-            bar.ShowDialog();
+            filp.Source.Add(new Bitmap("C:\\Users\\w\\Desktop\\VIF1COK36DW43[TC@F[((V8.png"));
+            filp.Source.Add(new Bitmap("C:\\Users\\w\\Desktop\\91523683_p8_master1200.jpg"));
+            filp.Source.Add(new Bitmap("C:\\Users\\w\\Desktop\\QQ20230402133547.jpg"));
         }
     }
 }
