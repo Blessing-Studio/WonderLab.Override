07d1bbe4134f0ebc1e14fa1d2e49404ba86058ea
YangSpring114 <blessing_114514@qq.com>
2023-06-23 13:08:54 +0800

    去了一个傻逼抛异常得地方

--- wonderlab/App.axaml.cs
+++ wonderlab/App.axaml.cs
@@ -25,7 +25,6 @@ namespace wonderlab {
         public override void Initialize() {
             AppDomain.CurrentDomain.UnhandledException += OnUnhandledException; ;
             AvaloniaXamlLoader.Load(this);
-            throw new NotImplementedException("114514");
         }
 
         private async void OnUnhandledException(object sender, UnhandledExceptionEventArgs e) {
