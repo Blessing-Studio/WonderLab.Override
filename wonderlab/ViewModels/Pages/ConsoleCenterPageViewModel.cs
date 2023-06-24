b46679896cc812811eb56bf766a57ee27136a53c
JWJUN233233 <1306334428@qq.com>
2023-06-24 21:47:19 +0800

    进程中心返回按钮bug修复

---
+++ wonderlab/ViewModels/Pages/ConsoleCenterPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wonderlab.Views.Pages;
+using wonderlab.Views.Windows;
+
+namespace wonderlab.ViewModels.Pages
+{
+    public class ConsoleCenterPageViewModel : ViewModelBase
+    {
+        public ConsoleCenterPageViewModel()
+        {
+
+        }
+    }
+}
--- wonderlab/Views/Pages/ConsoleCenterPage.axaml.cs
+++ wonderlab/Views/Pages/ConsoleCenterPage.axaml.cs
@@ -1,9 +1,12 @@
 using Avalonia.Controls;
+using wonderlab.ViewModels.Pages;
 
 namespace wonderlab.Views.Pages {
     public partial class ConsoleCenterPage : UserControl {
+        public static ConsoleCenterPageViewModel  ViewModel { get; set; }
         public ConsoleCenterPage() {
             InitializeComponent();
+            DataContext = ViewModel = new();
         }
     }
 }
