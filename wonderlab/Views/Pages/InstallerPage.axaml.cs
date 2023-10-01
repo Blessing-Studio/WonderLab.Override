4a4dfa5cbdccb9d711874ad25d22bacdd003cf20
YangSpring114 <blessing_114514@qq.com>
2023-10-01 23:19:07 +0800

    添加安装器页面

---
+++ wonderlab/Views/Pages/InstallerPage.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace wonderlab.Views.Pages {
+    public partial class InstallerPage : UserControl {
+        public InstallerPage() {
+            InitializeComponent();
+        }
+    }
+}
