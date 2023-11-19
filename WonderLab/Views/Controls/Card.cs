eeb3bbcf1075d4ae0d06a45bbab4ec53f770d702
YangSpring114 <blessing_114514@qq.com>
2023-11-19 14:59:44 +0800

    添加更多ui

---
+++ WonderLab/Views/Controls/Card.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls.Primitives;
+using System.Windows.Input;
+
+namespace WonderLab.Views.Controls {
+    public class Card : TemplatedControl {
+        public static readonly StyledProperty<IEnumerable> CardItemsProperty =
+            AvaloniaProperty.Register<NavigationView, IEnumerable>(nameof(CardItems), new AvaloniaList<NavigationViewItem>());
+
+        public static readonly StyledProperty<string> HeaderProperty =
+            AvaloniaProperty.Register<FontIcon, string>(nameof(Header), "Hello Header!");
+
+        public string Header { 
+            get => GetValue(HeaderProperty); 
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public IEnumerable CardItems {
+            get => GetValue(CardItemsProperty);
+            set => SetValue(CardItemsProperty, value);
+        }
+    }
+
+    public class CardItem : ListBoxItem {
+        public static readonly StyledProperty<string> HeaderProperty =
+            AvaloniaProperty.Register<FontIcon, string>(nameof(Header), "Hello Header!");
+
+        public string Header {
+            get => GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+    }
+}
---
+++ WonderLab/Views/Pages/Settings/LaunchSettingPage.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace WonderLab.Views.Pages.Settings {
+    public partial class LaunchSettingPage : UserControl {
+        public LaunchSettingPage() {
+            InitializeComponent();
+        }
+    }
+}
