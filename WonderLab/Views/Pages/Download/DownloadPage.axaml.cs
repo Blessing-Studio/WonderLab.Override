0defade86326398e552ed6885568471563a454dc
YangSpring114 <blessing_114514@qq.com>
2023-12-02 14:28:08 +0800

    添加新页面

---
+++ WonderLab/Views/Pages/Download/DownloadPage.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace WonderLab.Views.Pages.Download {
+    public partial class DownloadPage : UserControl {
+        public DownloadPage() {
+            InitializeComponent();
+        }
+    }
+}
