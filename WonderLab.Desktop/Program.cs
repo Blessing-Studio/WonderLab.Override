2bce32f59d429983edaa59f9617a48d47dbd27e2
YangSpring114 <blessing_114514@qq.com>
2024-03-22 21:31:36 +0800

    优化大部分控件不同主题的呈现效果

--- WonderLab.Desktop/Program.cs
+++ WonderLab.Desktop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Avalonia;
 using WonderLab.Extensions;
 
@@ -10,9 +11,19 @@ public sealed class Program {
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .UseSystemFont()
-            .LogToTrace();
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+        .UsePlatformDetect()
+        .UseSystemFont()
+        .LogToTrace()
+        .With(new Win32PlatformOptions() {
+            RenderingMode = RuntimeInformation.ProcessArchitecture == Architecture.Arm || RuntimeInformation.ProcessArchitecture == Architecture.Arm64 
+            ? [Win32RenderingMode.Wgl] 
+            : [Win32RenderingMode.AngleEgl, Win32RenderingMode.Software]!,
+        })
+        .With(new MacOSPlatformOptions() {
+            DisableDefaultApplicationMenuItems = true,
+        })
+        .With(new X11PlatformOptions() { 
+            OverlayPopups = true,
+        });
 }
