e633125a63a5dd604b2020e7b61bd28ad8ff4e5c
YangSpring114 <blessing_114514@qq.com>
2024-04-15 21:35:39 +0800

    完善关于页面

--- WonderLab/ViewModels/Pages/Setting/AboutPageViewModel.cs
+++ WonderLab/ViewModels/Pages/Setting/AboutPageViewModel.cs
@@ -1,3 +1,15 @@
-﻿namespace WonderLab.ViewModels.Pages.Setting;
+﻿using CommunityToolkit.Mvvm.Input;
+using Flurl;
+using System.Diagnostics;
 
-public sealed class AboutPageViewModel : ViewModelBase;
+namespace WonderLab.ViewModels.Pages.Setting;
+
+public sealed partial class AboutPageViewModel : ViewModelBase {
+    [RelayCommand]
+    private void JumpToLink(string url) {
+        using var process = Process.Start(new ProcessStartInfo(url) {
+            UseShellExecute = true,
+            Verb = "open"
+        });
+    }
+}
