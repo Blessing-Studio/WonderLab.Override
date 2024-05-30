9079d6bf09d2ea8a0c2b81e07a3a74e4a4ec504c
YangSpring114 <blessing_114514@qq.com>
2024-05-30 22:39:56 +0800

    完成测试版验证逻辑

--- WonderLab/ViewModels/Dialogs/TestUserCheckDialogViewModel.cs
+++ WonderLab/ViewModels/Dialogs/TestUserCheckDialogViewModel.cs
@@ -1,4 +1,36 @@
-﻿namespace WonderLab.ViewModels.Dialogs;
+﻿using Avalonia.Controls.Notifications;
+using CommunityToolkit.Mvvm.Input;
+using Flurl.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WonderLab.Classes.Datas.ViewData;
+using WonderLab.Services;
+
+namespace WonderLab.ViewModels.Dialogs;
 
 public sealed partial class TestUserCheckDialogViewModel : ViewModelBase {
+    private readonly SettingService _settingService;
+    private readonly NotificationService _notificationService;
+    private readonly string _apiKey = "9e15a18a-e726-453b-9004-a670c1dfaca3";
+
+    public TestUserCheckDialogViewModel(NotificationService notificationService, SettingService settingService) {
+        _settingService = settingService;
+        _notificationService = notificationService;
+    }
+
+    [RelayCommand]
+    private async Task Authenticate(string uuid) {
+        var result = await "http://47.113.149.130:14514/api/user".WithHeaders(new Dictionary<string, string>() {
+                { "x-api-key", _apiKey },
+                { "x-user-uuid", uuid },
+            }).GetJsonAsync<KeyValuePair<string, string>>();
+
+        _notificationService.QueueJob(new NotificationViewData {
+            Title = "你好",
+            Content = $"欢迎测试用户 {result.Value} 回来！",
+            NotificationType = NotificationType.Success
+        });
+
+        _settingService.Data.TestUserUuid = result.Key;
+    }
 }
--- WonderLab/Views/Controls/DialogContentPanel.cs
+++ WonderLab/Views/Controls/DialogContentPanel.cs
@@ -1,9 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia;
+using System.Windows.Input;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using WonderLab.Services.UI;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WonderLab.Views.Controls;
-internal class DialogContentPanel {
+
+public sealed class DialogContentPanel : TemplatedControl {
+    private Button _closeButton;
+
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<DialogContentPanel, string>(nameof(Title));
+
+    public static readonly StyledProperty<object> ContentProperty =
+        AvaloniaProperty.Register<DialogContentPanel, object>(nameof(Content));
+
+    public static readonly StyledProperty<bool> IsCloseButtonVisibleProperty =
+        AvaloniaProperty.Register<DialogContentPanel, bool>(nameof(IsCloseButtonVisible));
+
+    public static readonly StyledProperty<bool> IsPrimaryButtonVisibleProperty =
+        AvaloniaProperty.Register<DialogContentPanel, bool>(nameof(IsPrimaryButtonVisible));
+
+    public static readonly StyledProperty<object> PrimaryButtonContentProperty =
+        AvaloniaProperty.Register<DialogContentPanel, object>(nameof(PrimaryButtonContent));
+
+    public static readonly StyledProperty<ICommand> PrimaryButtonCommandProperty =
+        AvaloniaProperty.Register<DialogContentPanel, ICommand>(nameof(PrimaryButtonCommand));
+
+    public static readonly StyledProperty<object> PrimaryButtonCommandParameterProperty =
+        AvaloniaProperty.Register<DialogContentPanel, object>(nameof(PrimaryButtonCommandParameter));
+
+    public string Title {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public object Content {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+
+    public bool IsCloseButtonVisible {
+        get => GetValue(IsCloseButtonVisibleProperty);
+        set => SetValue(IsCloseButtonVisibleProperty, value);
+    }
+
+    public bool IsPrimaryButtonVisible {
+        get => GetValue(IsPrimaryButtonVisibleProperty);
+        set => SetValue(IsPrimaryButtonVisibleProperty, value);
+    }
+
+    public object PrimaryButtonContent {
+        get => GetValue(PrimaryButtonContentProperty);
+        set => SetValue(PrimaryButtonContentProperty, value);
+    }
+
+    public ICommand PrimaryButtonCommand {
+        get => GetValue(PrimaryButtonCommandProperty);
+        set => SetValue(PrimaryButtonCommandProperty, value);
+    }
+
+    public object PrimaryButtonCommandParameter {
+        get => GetValue(PrimaryButtonCommandParameterProperty);
+        set => SetValue(PrimaryButtonCommandParameterProperty, value);
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e) {
+        base.OnLoaded(e);
+
+        var dialogService = App.ServiceProvider.GetService<DialogService>();
+        _closeButton.Click += (_, _) => {
+            dialogService.CloseContentDialog();
+        };
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+        base.OnApplyTemplate(e);
+
+        _closeButton = e.NameScope.Find<Button>("closeButton");
+    }
 }
