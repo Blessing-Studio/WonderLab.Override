eeff1ed9733321ae8a255b6778b9ccf24b0d97a4
YangSpring114 <blessing_114514@qq.com>
2024-07-22 17:14:53 +0800

    尝试更换 osx 版 CI 构建方式

--- .build/build_osx.sh
+++ .build/build_osx.sh
@@ -3,10 +3,49 @@
 file_content=$(cat ./WonderLab/WonderLab.csproj)
 branch=$(echo "$file_content" | grep -oPm1 "(?<=<Branch>)[^<]+")
 version=$(echo "$file_content" | grep -oPm1 "(?<=<Version>)[^<]+")
+runtime=$(echo "$file_content" | grep -oPm1 "(?<=<TargetFramework>)[^<]+")
 
-echo "Version: $version"
 echo "Branch: $branch"
+echo "Version: $version"
+echo "RunTime: $runtime"
 
+# WonderLab.Desktop.2.0.0.osx-x64.zip
+# bin\Release\net8.0\publish\osx-arm64\
 build_osx() {
-    echo "build WonderLab-"
+    echo "build WonderLab.$branch.$version.$1.app.zip"
+
+    base_dir="./WonderLab.Desktop/bin/Release/$runtime/publish/$1" 
+    base_app_dir="$base_dir/WonderLab.app/Contents"
+    zip_name="WonderLab.$branch.$version.$1.app.zip"
+
+    dotnet publish ./WonderLab.Desktop -p:PublishProfile=$1
+
+    mkdir $base_dir/WonderLab.app
+    mkdir $base_app_dir
+
+    resources_dir=$base_app_dir/Resources
+
+    mkdir $resources_dir
+
+    cp ./WonderLab.Desktop/Info.plist $base_app_dir
+    cp ./WonderLab.Desktop/wonderlab.icns $resources_dir
+
+    files=("libAvaloniaNative.dylib"
+    "libHarfBuzzSharp.dylib"
+    "libSkiaSharp.dylib"
+    "WonderLab.Desktop")
+
+    app_dir="$base_app_dir/MacOS"
+    mkdir $app_dir
+
+    for file in ${files[@]}
+    do
+        cp $base_dir/$file $app_dir/$file
+    done
+
+    chmod a+x $app_dir/WonderLab.Desktop
+    zip $zip_name $base_dir/WonderLab.app
+    echo "$zip_name build done!"
 }
+
+build_osx osx-x64
