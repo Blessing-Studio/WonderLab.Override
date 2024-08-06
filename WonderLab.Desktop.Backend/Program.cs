ce68396469c736f446639560c16a615eaf4f41f2
YangSpring114 <blessing_114514@qq.com>
2024-08-07 00:53:22 +0800

    添加后台任务处理程序

---
+++ WonderLab.Desktop.Backend/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace WonderLab.Desktop.Backend;
+
+public class Program {
+    public static Task Main(string[] args) {
+        return Build().InvokeAsync(args);
+    }
+
+    private static RootCommand Build() {
+        var idOption = new Option<string>("-id", "game id.");
+        var threadOption = new Option<int>("-thread", "Maximum number of threads.");
+        var pathOption = new Option<string>("-path", "The path to the game directory.");
+        var sourceOption = new Option<string>("-source", "Default download source for resources.");
+
+        var root = new RootCommand("Backend time-consuming event handler.");
+
+        root.AddGlobalOption(idOption);
+        root.AddGlobalOption(pathOption);
+        root.AddGlobalOption(threadOption);
+        root.AddGlobalOption(sourceOption);
+
+        var subCommand = new Command("--install", "install games.");
+        subCommand.SetHandler(() => {
+            Console.WriteLine("Subcommand 1!");
+        });
+
+        var completion = new Command("--completion", "Complete game assets and libraries files.");
+        completion.SetHandler(async (id, path, source, thread) => {
+            await ResourceDownloader.CompleteResourceAsync(path, id, source, thread);
+        },
+        idOption, pathOption, sourceOption, threadOption);
+
+        root.Add(subCommand);
+        root.Add(completion);
+        return root;
+    }
+}
---
+++ WonderLab.Desktop.Backend/ResourceDownloader.cs
@@ -0,0 +1,35 @@
+﻿using MinecraftLaunch;
+using MinecraftLaunch.Extensions;
+using MinecraftLaunch.Components.Checker;
+using MinecraftLaunch.Components.Resolver;
+using MinecraftLaunch.Classes.Models.Download;
+
+namespace WonderLab.Desktop.Backend;
+public static class ResourceDownloader {
+    public static async Task CompleteResourceAsync(string path, string id, string source, int thread) {
+        var gameResolver = new GameResolver(path);
+        var checker = new ResourceChecker(gameResolver.GetGameEntity(id));
+        bool isNeedComplete = !await checker.CheckAsync();
+        Console.WriteLine($"{isNeedComplete}");
+        Console.WriteLine($"{checker.MissingResources.Count}");
+
+        if (isNeedComplete) {
+            await checker.MissingResources.DownloadResourceEntrysAsync(GetSource(source), x => {
+                Console.WriteLine($"[{x.CompletedCount}/{x.TotalCount}][{x.ToPercentage() * 100:0.00}%]");
+            }, new() {
+                MultiPartsCount = 8,
+                MultiThreadsCount = thread,
+                FileSizeThreshold = 1024 * 1024 * 3,
+                IsPartialContentSupported = true
+            });
+        }
+    }
+
+    private static MirrorDownloadSource GetSource(string source) {
+        if (source == "bmcl") {
+            return MirrorDownloadManager.Bmcl;
+        } else {
+            return default!;
+        }
+    }
+}
